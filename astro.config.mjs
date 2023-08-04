611a2769bb2490283ea3fcb5d95bac50ce9de65d
Sabbir <61304556+sabbir-d@users.noreply.github.com>
2023-08-05 00:28:57 +0600

    ff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from 'vite';
+import tailwind from "@astrojs/tailwind";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: "storyblok/Page",
+      introduction: "storyblok/Introduction",
+      about: "storyblok/About",
+      experience: "storyblok/Experience",
+      singleExperience: "storyblok/SingleExperience",
+      project: "storyblok/Project",
+      singleProject: "storyblok/SingleProject",
+      testimonial: "storyblok/Review",
+      review: "storyblok/SingleReview",
+      contact: "storyblok/Contact"
+    }
+  }), tailwind()]
+});
